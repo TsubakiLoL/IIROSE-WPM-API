f0e0b7bbf9a424c2e872786230db6ce75fcbfeef
Tsubaki^O^ <163342432+TsubakiLoL@users.noreply.github.com>
2024-11-06 10:08:16 +0800

    Update main.js

--- main.js
+++ main.js
@@ -41,7 +41,7 @@ function message_get(p){
     
 }
 
-socket._onmessage = proxyFunction(socket._onmessage.bind(socket), async (p) => {console.log(p)});
+socket._onmessage = proxyFunction(socket._onmessage.bind(socket), async (p) => {message_get(p)});
 
 //IIROSEAPI.send_bullet_message("test");
 
