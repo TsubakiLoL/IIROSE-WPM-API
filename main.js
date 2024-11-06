1df50b89fd2193bd360b158e42b8701ff5d33c2d
Tsubaki^O^ <163342432+TsubakiLoL@users.noreply.github.com>
2024-11-06 09:36:57 +0800

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,29 @@ class IIROSEAPI {
         };
         socket.send(JSON.stringify(msg_json))
     }
+    static send_bullet_message(msg){
+        //~{"t":"(消息内容)","c":"(消息颜色)","v":0}
+        let msg_json = {
+            "t": msg.toString(),
+            "c": inputcolorhex,
+            "v": 0
+        };
+        socket.send("~"+JSON.stringify(msg_json))
+    }
+    static send_private_message(msg,uid){
+        //{"g":"(接受者用户唯一标识)","m":"(消息内容)","mc":"(消息颜色)","i":"(随机数，房间消息的示例)"}
+        let msg_json = {
+            "g": uid.toString,
+            "m": msg.toString(),
+            "mc": inputcolorhex,
+            "i": Math.random().toString().slice(2, 14)
+        };
+        socket.send(JSON.stringify(msg_json))
+        
+    }
+    
 }
 
-IIROSEAPI.send_room_message("test");
+
+IIROSEAPI.send_bullet_message("test");
 
