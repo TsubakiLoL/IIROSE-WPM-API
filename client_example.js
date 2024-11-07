e0ba85798be9dd56c0201099689815bef8e7c2b0
Tsubaki^O^ <163342432+TsubakiLoL@users.noreply.github.com>
2024-11-07 22:49:31 +0800

    Update client_example.js

--- client_example.js
+++ client_example.js
@@ -41,7 +41,7 @@ async function start(){
 
   // 创建iframe
   var el = document.createElement('iframe');
-  el.src = "https://like.tsubaki.fun";
+  el.src = "https://js.tsubaki.fun";
   el.width = "1152px";
   el.height = "648px";
   el.style.border = 'none'; // 移除iframe的边框
@@ -54,11 +54,8 @@ async function start(){
   // 将悬浮窗容器添加到文档body中
   document.body.appendChild(floatingWindow);
   //添加监听
-  function dealMessage(e) {
-    console.log(e.data);
-  }
+  
   IIROSEAPI.add_new_plugin("https://like.tsubaki.fun",el.contentWindow);
-  el.contentWindow.addEventListener('message', dealMessage, false);
 
 }
 
